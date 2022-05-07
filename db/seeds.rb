f57bdf0378245bdadb8e71f42b354663d76fb6de
Kingsley Ibe <43799727+KingsleyIbe@users.noreply.github.com>
2022-05-06 21:14:44 -1100

    Populate database with random greetings

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Greeting.create(message: 'Hello Friend')
+Greeting.create(message: 'Hello Microverse')
+Greeting.create(message: 'Hello Universe')
+Greeting.create(message: 'Hello Partner')
+Greeting.create(message: 'Hello Team')
+Greeting.create(message: 'Hello Charles')
+Greeting.create(message: 'Hello Kingsley')
+Greeting.create(message: 'Hello Amadu')
+Greeting.create(message: 'Hello Kevin')
+Greeting.create(message: 'Hello Anselem')
+Greeting.create(message: 'Hello Anuar')
+Greeting.create(message: 'Hello Students')
